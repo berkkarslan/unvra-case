dcf9cf9970e41fb080ac73a98728810e85487b39
Berk Arslan <berkarslan97@gmail.com>
2022-08-31 11:07:02 +0300

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      
+      <a>
+        <div>
+          <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" />
+        </div>
+      </a>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

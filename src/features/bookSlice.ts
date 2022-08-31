e185a60d9475a9584da3263ab57c9b06c490ff50
Berk Arslan <berkarslan97@gmail.com>
2022-08-31 14:24:00 +0300

    added redux

---
+++ src/features/bookSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { server } from '../api/server'
+
+const initialState: IStoreState = {
+  books: [],
+  status: 'idle',
+  error: null,
+}
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await server.req('/books', 'GET')
+  return response.data
+})
+
+export const addNewBook = createAsyncThunk(
+  'books/addNewBook',
+  async (book: Book) => {
+    const response = await server.req('/books', 'POST', book)
+    return response.data
+  }
+)
+
+export const deleteBook = createAsyncThunk(
+  'books/deleteBook',
+  async (id: string) => {
+    await server.req(`/books/${id}`, 'DELETE')
+    return id
+  }
+)
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchBooks.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.books = [...action.payload]
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+      .addCase(addNewBook.fulfilled, (state, action) => {
+        state.books.push(action.payload)
+      })
+
+      .addCase(deleteBook.fulfilled, (state, action) => {
+        state.books.splice(
+          state.books.findIndex((book: Book) => book.id === action.payload),
+          1
+        )
+      })
+  },
+})
+
+export default booksSlice.reducer
+
+export const selectAllBooks = (state: RootState): Book[] => state.books.books
--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,23 @@ import AddPage from './pages/AddPage'
 import BorrowingsPage from './pages/Borrowings'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<AddPage />} />
-          <Route path="/borrowings" element={<BorrowingsPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <main className="container mt-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddPage />} />
+            <Route path="/borrowings" element={<BorrowingsPage />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
